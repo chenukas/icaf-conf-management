c318cb0df0add12e5e7431b36b640cd2f05bd878
unknown <chenukasumanasekara@gmail.com>
2021-05-07 01:40:22 +0530

    Dashboard styling and sidenav

--- frontend/src/components/Sidenav/Sidenav.js
+++ frontend/src/components/Sidenav/Sidenav.js
@@ -1,26 +1,55 @@
 import React from "react";
+import "./Sidenav.css";
 import { useRouteMatch, Link } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
+import PersonIcon from "@material-ui/icons/Person";
+import PaymentIcon from "@material-ui/icons/Payment";
+import PublishIcon from "@material-ui/icons/Publish";
 
 const Sidenav = () => {
   let match = useRouteMatch();
 
   return (
-    <>
-      <ul>
-        <li>
-          <Link to={`${match.url}`}>Overview</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/users`}>Users</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/publications`}>Publications</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/payments`}>Payments</Link>
-        </li>
+    <div className="sidenav-container">
+      <br />
+      <ul className="sidebar-list">
+        <Link style={{ textDecoration: "none" }} to={`${match.url}`}>
+          <li className="sidebar-row">
+            <div id="icon">
+              <HomeIcon />
+            </div>{" "}
+            <div id="title">Overview</div>
+          </li>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to={`${match.url}/users`}>
+          <li className="sidebar-row">
+            <div id="icon">
+              <PersonIcon />
+            </div>{" "}
+            <div id="title">Users</div>
+          </li>
+        </Link>
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`${match.url}/publications`}
+        >
+          <li className="sidebar-row">
+            <div id="icon">
+              <PublishIcon />
+            </div>{" "}
+            <div id="title">Publications</div>
+          </li>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to={`${match.url}/payments`}>
+          <li className="sidebar-row">
+            <div id="icon">
+              <PaymentIcon />
+            </div>{" "}
+            <div id="title">Payments</div>
+          </li>
+        </Link>
       </ul>
-    </>
+    </div>
   );
 };
 
--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -5,20 +5,24 @@ import Overview from "../../components/Overview/Overview";
 import Users from "../../components/Users/Users";
 import Publications from "../../components/Publications/Publications";
 import Payments from "../../components/Payments/Payments";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   const match = useRouteMatch();
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Sidenav />
-
-      <Switch>
-        <Route exact path={`${match.url}`} component={Overview} />
-        <Route path={`${match.url}/users`} component={Users} />
-        <Route path={`${match.url}/publications`} component={Publications} />
-        <Route path={`${match.url}/payments`} component={Payments} />
-      </Switch>
+    <div className="dashboard">
+      <div>
+        <Sidenav />
+      </div>
+      <div>
+        <br />
+        <Switch>
+          <Route exact path={`${match.url}`} component={Overview} />
+          <Route path={`${match.url}/users`} component={Users} />
+          <Route path={`${match.url}/publications`} component={Publications} />
+          <Route path={`${match.url}/payments`} component={Payments} />
+        </Switch>
+      </div>
     </div>
   );
 };
