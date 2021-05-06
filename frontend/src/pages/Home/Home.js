8b5380952111e8f2725f9fc7e76c573fecdd33b4
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-06 09:29:21 +0530

    chore: moved dashboard and home to pages, others to components

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import Navbar from "./Navbar/navbar";
+
+const Home = () => {
+  return (
+    <Link to="/nav">Nav</Link>
+   
+  );
+};
+
+export default Home;
