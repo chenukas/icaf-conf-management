7e641481848448c0f2c9bf630f551e2690039a4e
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-08 23:05:36 +0530

    code: completed the navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
     return (
         <div className="nav">
             <div className="navbarContainer">
-                <h3 to="/" className="navLogo">FrameCon'21</h3>
+                <h3 to="/" className="navLogo">ICAF</h3>
+                <div className="mobileIcon" onClick={toggle}>
+                    <h1 className="faBars"/>
+                </div>
+                <div className="navMenu">
+                    <div className="navItem">
+                        <p className="navLinks">About</p>
+                    </div>
+                    <div className="navItem">
+                        <p className="navLinks">Discover</p>
+                    </div>
+                    <div className="navItem">
+                        <p className="navLinks">Services</p>
+                    </div>
+                    <div className="navItem">
+                        <p className="navLinks">Sign Up</p>
+                    </div>
+                    <div className="navBtn">
+                        <p className="navBtnLink">Sign In</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
