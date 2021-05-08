3c059857c03b526d02de01095d5b0991b58e0760
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-09 01:08:59 +0530

    code: changes in the hero component

--- frontend/src/components/LadingPage/Hero/Hero.js
+++ frontend/src/components/LadingPage/Hero/Hero.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import lady from '../../../images/conference.svg'
+ 
 const Hero = () => {
     return (
         <div className="heroContainer">
-            <div className="heroBg">
-                <src className="videoBg"/>
+            <div className="heroBg"> 
+                <div >
+                    <img className="image" style={{width: "30vw"}} src={lady} alt="listening to a conference "/>
+                </div>
             </div>
         </div>
     )
