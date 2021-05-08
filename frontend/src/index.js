d603f2f3dcb61608156da067f27ce5a5e98a7c0b
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-08 23:11:06 +0530

    code: changes in the home page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import '@fontsource/roboto';
 
 ReactDOM.render(
   <BrowserRouter>
