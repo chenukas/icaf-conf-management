3194f6507a1537b1aa4745510c3634724432eaf2
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-08 09:44:58 +0530

    adding path to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./components/Home/Home";
+import Home from "./components/Home/Home.js";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Users from "./components/Dashboard/Users";
+import Navbar from "./components/Home/Navbar/navbar";
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/nav" component={Navbar} />
         <Route exact path="/admin" component={Dashboard} />
         <Route path="/admin/users" component={Users} />
       </Switch>
