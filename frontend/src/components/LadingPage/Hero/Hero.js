0edc467f33ab7b3bcc18edc92dd7964c1f77270c
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-08 23:01:36 +0530

    chore: created hero component

---
+++ frontend/src/components/LadingPage/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className="heroContainer">
+            <div className="heroBg">
+                <src className="videoBg"/>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
