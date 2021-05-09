7488b37dd340328d8c5e33d8c65d9eb7b216ebbf
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-09 14:11:32 +0530

    chore: changes in the navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,38 +1,48 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 import logo from "../../images/logo.png";
 
-const Navbar = ({toggle}) => {
-    return (
-        <div className="nav">
-            <div className="navbarContainer">
-                <h3 to="/" className="navLogo">
-                <img style={{width: "5.5vw"}} src={logo} alt="logo"/>
-                    ICAF
-                </h3>
-                <div className="mobileIcon" onClick={toggle}>
-                    <p className="faBars"></p>
-                </div>
-                <div className="navMenu">
-                    <div className="navItem">
-                        <p className="navLinks" to="">About</p>
-                    </div>
-                    <div className="navItem">
-                        <p className="navLinks" to="">Discover</p>
-                    </div>
-                    <div className="navItem">
-                        <p className="navLinks" to="">Services</p>
-                    </div>
-                    <div className="navItem">
-                        <p className="navLinks" to="">Sign Up</p>
-                    </div>
-                    <div className="navBtn">
-                        <p className="navBtnLink" to="">Sign In</p>
-                    </div>
-                </div>
-            </div>
+const Navbar = ({ toggle }) => {
+  return (
+    <div className="nav">
+      <div className="navbarContainer">
+        <h3 to="/" className="navLogo">
+          <img style={{ width: "5.5vw" }} src={logo} alt="logo" />
+          ICAF
+        </h3>
+        <div className="mobileIcon" onClick={toggle}>
+          <p className="faBars"></p>
         </div>
-    );
+        <div className="navMenu">
+          <div className="navItem">
+            <p className="navLinks" to="">
+              About
+            </p>
+          </div>
+          <div className="navItem">
+            <p className="navLinks" to="">
+              Discover
+            </p>
+          </div>
+          <div className="navItem">
+            <p className="navLinks" to="">
+              Services
+            </p>
+          </div>
+          <div className="navItem">
+            <p className="navLinks" to="">
+              Sign Up
+            </p>
+          </div>
+          <div className="navBtn">
+            <p className="navBtnLink" to="">
+              Sign In
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
