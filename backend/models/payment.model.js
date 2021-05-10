ba269bfc331c4de27ba748502c30a22537436c5c
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2021-05-10 15:56:45 +0530

    Modified payment model class

--- backend/models/payment.model.js
+++ backend/models/payment.model.js
@@ -3,12 +3,13 @@ const {Schema} = mongoose;
 
 const paymentSchema = new Schema(
     {
+        paymentId : { type: String, require: true },
         type : { type: String, require: true },
         userID : { type: String, require: true },
         userName : { type: String, require: true },
         payDate : { type: Date, require: true },
         amount : { type: Number, require: true }
     }
-)
+);
 
 module.exports = mongoose.model("Payment", paymentSchema);
