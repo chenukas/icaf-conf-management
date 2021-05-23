68f779af4a51cda91e21f9ba033d033d44d75444
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-05-23 20:34:39 +0530

    changes in hero component

--- frontend/src/components/LadingPage/Hero/Hero.js
+++ frontend/src/components/LadingPage/Hero/Hero.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import lady from '../../../images/conference.svg'
- 
-const Hero = () => {
-    return (
-        <div className="heroContainer">
-            <div className="heroBg"> 
-                <div >
-                    <img className="image" style={{width: "30vw"}} src={lady} alt="listening to a conference "/>
-                </div>
-            </div>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import lady from "../../../images/conference.svg";
 
-export default Hero
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <div className="heroContainer row">
+      <div className="col">
+        <div>
+          <img
+            className="image"
+            style={{ width: "30vw" }}
+            src={lady}
+            alt="listening to a conference"
+          />
+        </div>
+      </div>
+      <div className="col">
+        <div className="heroH1">Topic of th Conference</div>
+        <div className="heroP">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud
+        </div>
+        <div className="heroBtnWrapper">
+          <div to="/#" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started 
+            {/* {hover ? <arrowForward /> : <arrowRight />} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
