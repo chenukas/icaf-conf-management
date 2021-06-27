0547a59862e800d107bf18b4995ced33cd9b4ffc
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-27 20:24:56 +0530

    added new components

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Hero from "../../components/Landing/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
 import Timeline from "../../components/Landing/Timeline/Timeline";
+import AboutUs from "../../components/Landing/AboutUs/AboutUs";
+import Speakers from "../../components/Landing/Speakers/Speakers";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <AboutUs />
+      <Speakers />
       <Timeline />
     </>
   );
