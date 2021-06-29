efc79227bc189e9d0d392ea1d34ceea7f1ca1b0b
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-29 09:42:25 +0530

    completed the model

--- backend/models/submission.model.js
+++ backend/models/submission.model.js
@@ -4,13 +4,12 @@ const { Schema } = mongoose;
 const submissionSchema = new Schema(
   {
     title: { type: String, require: true },
-    abstract: { type: Date, require: true },
+    abstract: { type: String, require: true },
     authors: [
       {
         type: Schema.Types.ObjectId,
       },
     ],
-    title: { type: String, require: true },
     fileURL: { type: String, require: true },
     status: { type: String, require: true },
   },
