f186cc8a268eb056c25b8041e14135086c634168
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-30 02:35:13 +0530

    created submissions componenet

---
+++ frontend/src/components/Submissions/Submissions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Submissions.css";
+
+const Submissions = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Submissions
