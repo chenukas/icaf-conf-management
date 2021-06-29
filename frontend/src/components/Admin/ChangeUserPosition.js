990e079660652cabf71904d5ee0095ff772be35f
RavinduDilhara <ravindu.1dilhara@gmail.com>
2021-06-29 11:11:11 +0530

    change-position

--- frontend/src/components/Admin/ChangeUserPosition.js
+++ frontend/src/components/Admin/ChangeUserPosition.js
@@ -1,63 +1,79 @@
 import React, { Component } from "react";
 import { UncontrolledPopover, PopoverBody } from "reactstrap";
-import { Link } from "react-router-dom";
+import "./ChangeUser.css";
+import axios from "axios";
+import swal from "sweetalert";
 
 class ChangeUserPosition extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      id: "",
       fullName: "",
       email: "",
-      password: "",
       contactNumber: "",
       position: 0,
-      type: "",
-      confirmPassword: "",
     };
   }
 
-  onChangeUserName = (e) => {
-    this.setState({
-      fullName: e.target.value,
-    });
-  };
-  onChangeContactNumber = (e) => {
-    this.setState({
-      contactNumber: e.target.value,
-    });
-  };
-  onChangeEmail = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  };
-  onChangePassword = (e) => {
-    this.setState({
-      password: e.target.value,
-    });
-  };
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/viewUser/" + this.props.match.params.id)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          id: response.data._id,
+          fullName: response.data.fullName,
+          email: response.data.email,
+          contactNumber: response.data.contactNumber,
+          position: response.data.position,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   onChangePosition = (e) => {
     this.setState({
-      type: e.target.value,
+      position: e.target.value,
     });
   };
-  onChangeCnfPassword = (e) => {
-    this.setState({
-      confirmPassword: e.target.value,
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      id: this.state.id,
+      position: this.state.position,
+    };
+
+    axios.put("http://localhost:5000/updatePosition", user).then((res) => {
+      if (res.data.success === true) {
+        window.location = "/dashboard/users";
+      }
+      if (res.data.success === false) {
+        swal({
+          title: "Wrong Registration !",
+          text: res.data.message,
+          icon: "error",
+          button: true,
+          dangerMode: true,
+        });
+      }
     });
   };
 
   render() {
     return (
-      <div className="container" style={{ marginTop: 50, marginLeft: 30 }}>
-        <div style={{ width: "75%", marginTop: 50, marginLeft: 500 }}>
-          <h1>CHANGE USER POSITION</h1>
+      <div className="change-container">
+        <div className="change-header">
+          <h2>Change User Position</h2>
         </div>
         <form
           onSubmit={this.onSubmit}
           className="jumbotron"
-          style={{ backgroundColor: "#F8DE7E" }}
+          style={{ backgroundColor: "#fff", marginLeft: "5%" }}
         >
           <div className="form-group">
             <label>Full Name: </label>
@@ -120,7 +136,7 @@ class ChangeUserPosition extends Component {
                   data-content="And here's some amazing content. It's very engaging. Right?"
                   style={{
                     color: "#fff",
-                    backgroundColor: "#0097A7",
+                    backgroundColor: "#01bf71",
                     fontWeight: "bold",
                     width: 400,
                   }}
