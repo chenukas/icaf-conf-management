3bb15bc6300ec806eda133964f7aec2c3df6117f
Suhara Vithanage <suha.lyoko@hotmail.com>
2021-06-29 09:53:03 +0530

    Added the  routes

--- backend/routes/submission.routes.js
+++ backend/routes/submission.routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const submissionController = require("../controllers/submission.controller");
 
 router.post("/submissions", submissionController.addSubmission);
-router.get("/submissions", submissionController.viewAllSubmissions);
-router.get("/submissions/:id", submissionController.viewSubmission);
-router.delete('/submissions/:id', submissionController.deleteSubmission);
+router.get("/submissions", submissionController.getSubmissions);
+router.get("/submissions/:id", submissionController.viewSubmissionById);
+router.delete('/submissions/:id', submissionController.deleteSubmissionById);
 
 module.exports = router;
